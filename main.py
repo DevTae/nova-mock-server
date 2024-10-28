d4217b53b3f42286a26443169d5104d540e4036a
DevTae <dev.taehyeon.kim@gmail.com>
2024-10-28 22:37:34 +0900

    Update main.py to return multiple states randomly

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ from datetime import datetime
 
 app = FastAPI() # python3 -m uvicorn main:app --reload
 
+count = 0
+
 @app.get("/v1/nodes")
 async def get_nodes():
+    global count
+
     timestamp = datetime.utcnow().isoformat()
     
+    count += 1
+    if count % 5 == 0:
+        state = "cleaning"
+    else:
+        state = "manageable"
+
     response_data = {
             "time": timestamp,
-            "state": "manageable"
+            "state": state
     }
 
+    return response_data
+
+@app.post("/v1/update")
+async def update_metrics():
+    response_data = {}
+    
     return response_data
 
